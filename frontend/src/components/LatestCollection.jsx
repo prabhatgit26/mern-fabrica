2d26f94b1580e0fc750b23e50f9682e72b2054ec
prabhatgit26 <prabhatwebdev26@gmail.com>
2024-09-25 18:01:12 +0530

    frontend updated 25 sept24

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,36 @@
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+const LatestCollection = () => {
+  const { products } = useContext(ShopContext);
+  const [latestproduct, setLatestproduct] = useState([]);
+
+  useEffect(() => {
+    setLatestproduct(products.slice(0, 10));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTIONS"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          A seasonal collection highlighting fall fashion trends, rich textures,
+          and warm tones.
+        </p>
+      </div>
+
+      {/* Rendering Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {
+            latestproduct.map((item, index)=>(
+                <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+            ))
+        }
+      </div>
+    </div>
+  );
+};
+
+export default LatestCollection;
