2f80ec98fb74e142c7f43d3cb05b5e48dafddb98
prabhatgit26 <prabhatwebdev26@gmail.com>
2024-09-29 00:02:08 +0530

    updated frontend 28 sept.

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,20 +2,21 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProduct from "../components/RelatedProduct";
 
 const Product = () => {
   const { productId } = useParams(); //we will get product id
-  const { products, currency } = useContext(ShopContext); // we will get all products data from context
+  const { products, currency, addToCart } = useContext(ShopContext); // we will get all products data from context
   const [productData, setProductdata] = useState(false);
   const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
 
   const fetchProductData = async () => {
     products.map((item) => {
       if (item._id === productId) {
         setProductdata(item);
         setImage(item.image[0]);
-        console.log(item);
-        
+
         return null;
       }
     });
@@ -32,43 +33,104 @@ const Product = () => {
         {/* --------------- product images ------------------- */}
         <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
           <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
-            {
-              productData.image.map((item, index)=>(
-                <img onClick={()=>setImage(item)} src={item} key={index} alt="" className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"/>
-              ))
-            }
+            {productData.image.map((item, index) => (
+              <img
+                onClick={() => setImage(item)}
+                src={item}
+                key={index}
+                alt=""
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+              />
+            ))}
           </div>
           <div className="w-full sm:w-[80%]">
-              <img src={image} className="w-full h-auto" alt="" />
+            <img src={image} className="w-full h-auto" alt="" />
           </div>
         </div>
 
         {/* ------------Product information ------------ */}
-            <div className="flex-1 ">
-              <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
-              <div className="flex items-center gap-1 mt-2">
-                <img src={assets.star_icon} alt="" className="w-3 5" />
-                <img src={assets.star_icon} alt="" className="w-3 5" />
-                <img src={assets.star_icon} alt="" className="w-3 5" />
-                <img src={assets.star_icon} alt="" className="w-3 5" />
-                <img src={assets.star_dull_icon} alt="" className="w-3 5" />
-                <p className="pl-2">(122)</p>
-              </div>
-              <p className="mt-5 text-3xl font-medium">{currency}{productData.price}</p>
-              <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
-              <div className="flex flex-col gap-4 my-8">
-                  <p>Select Size</p>
-                  <div className="flex gap-2">
-                    {
-                      productData.sizes.map((item, index)=>(
-                        <button className={`border py-2 px-4 bg-gray-100`} key={index}>{item}</button>
-                      ))
-                    }
-                  </div>
-              </div>
+        <div className="flex-1 ">
+          <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
+          <div className="flex items-center gap-1 mt-2">
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+            <p className="pl-2">(122)</p>
+          </div>
+          <p className="mt-5 text-3xl font-medium">
+            {currency}
+            {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`border py-2 px-4 bg-gray-100 ${
+                    item === size ? " bg-gray-600 text-white" : ""
+                  }`}
+                  key={index}
+                >
+                  {item}
+                </button>
+              ))}
             </div>
-
+          </div>
+          <button
+            onClick={() => addToCart(productData._id, size)}
+            className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"
+          >
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original Product.</p>
+            <p>Cash On Delivery is Available on this Product.</p>
+            <p>Easy return & exchange policy in 7 Days.</p>
+          </div>
+        </div>
       </div>
+
+      {/* ---------- Description and review section ------------------ */}
+      <div className="mt-20">
+        <div className="flex ">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border py-6 px-6 text-sm text-gray-700">
+          <p>
+            An E-commerce clothing website offers a diverse collection of trendy
+            apparel for men, women, and children. With a user-friendly
+            interface, customers can easily browse through categories, filter by
+            size and style, and enjoy a seamless shopping experience. The
+            platform features high-quality product images, detailed
+            descriptions, and secure payment options, ensuring a convenient and
+            enjoyable online shopping journey. Additionally, customers can take
+            advantage of special promotions and personalized recommendations to
+            find their perfect outfit.
+          </p>
+
+          <p>
+            E-commerce clothing site allows users to easily browse and filter
+            products, add items to the cart, and securely complete purchases. It
+            supports user authentication, order tracking, and personalized
+            recommendations for a smooth shopping experience.
+          </p>
+        </div>
+      </div>
+
+      {/* ----------- display related products --------------- */}
+
+      <RelatedProduct
+        category={productData.category}
+        subCategory={productData.subCategory}
+      />
     </div>
   ) : (
     <div className="opacity-0"></div>
